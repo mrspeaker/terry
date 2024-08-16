bcde431b4b1d0426f11fe0cb42668ea95fad7849
Mr Speaker <mrspeaker@gmail.com>
2024-08-17 07:23:54 +0800

    c snake

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <fcntl.h>
+#include <termios.h>
 
 void esc(char* str) {
     printf("\e[%s", str);
@@ -16,14 +17,24 @@ struct winsize win;
 int stdoutid = STDOUT_FILENO;
 unsigned long twinsz = TIOCGWINSZ;
 
+void nonblock(int enable) {
+    struct termios ttystate;
+    tcgetattr(STDIN_FILENO, &ttystate); // save current state
+    if (enable) {
+        ttystate.c_lflag &= ~ICANON; // cannonical
+        ttystate.c_cc[VMIN] = 1; // minimum of number input read.
+    }  else {
+        ttystate.c_lflag |= ICANON;
+    }
+    tcsetattr(STDIN_FILENO, TCSANOW, &ttystate);
+}
+
 void init() {
     system("stty -echo"); //don't echo
-    system("stty -icanon");
+    nonblock(1);
     esc("48;5;0m"); // bg black
     esc("2J"); // clear screen
     esc("?25l"); // hide cursor
-
-    esc("> 1 u");
 }
 
 void done(int signum) {
@@ -31,13 +42,24 @@ void done(int signum) {
     esc("37m"); // fg to white
     esc("48;5;0m"); // bg to black
 
-    esc("< u");
-    esc("?1003l");
     system("stty echo"); //turn on screen echo again
+    nonblock(0);
+
     printf("\nbye\n");
 
     exit(signum);
 };
+
+int kbhit() {
+    struct timeval tv;
+    fd_set fds;
+    tv.tv_sec = 0;
+    tv.tv_usec = 0;
+    FD_ZERO(&fds);
+    FD_SET(STDIN_FILENO, &fds);
+    select(STDIN_FILENO+1, &fds, NULL, NULL, &tv);
+    return FD_ISSET(STDIN_FILENO, &fds);
+}
 
 void resize() {
     init();
@@ -46,54 +68,49 @@ void resize() {
     h = win.ws_row;
 }
 
-
 int main() {
     signal(SIGINT, done);
     signal(SIGWINCH, resize);
 
-    /*int fd = open("/dev/input/event0", O_RDONLY);
-    if (fd == -1) {
-        perror("Error opening input device");
-        return 1;
-    }
-    close(fd);*/
-
     resize();
 
-    char x = 0;
-    char y = 0;
+    char x = w / 2;
+    char y = h  / 2;
+    char dx = 0;
+    char dy = 0;
 
     int delay = 1000000 / 30;
-    char t = 0;
+    unsigned char t = 0;
 
-    esc("?1003h");
+    char ch;
+    int running = 1;
+    while(running){
+        // Input
+        if (kbhit()) {
+            dx = 0;
+            dy = 0;
+            ch = fgetc(stdin);
+            if (ch == 'q') running = 0;
+            if (ch == 'w') dy = -1;
+            if (ch == 's') dy = 1;
+            if (ch == 'd') dx = 1;
+            if (ch == 'a') dx = -1;
+        }
 
-    char buffer[16] = " ";
-    int flags = fcntl(stdin->_fileno, F_GETFL, 0);
-    fcntl(stdin->_fileno, F_SETFL, flags | O_NONBLOCK);
-
-    while(1){
+        // Update
         t++;
 
-        x++;
+        x += dx;
+        if (x < 0) x = w;
         if (x > w) x = 0;
 
-        //ioctl(stdoutid, KDGETKEYCODE, &kb);
+        y += dy;
+        if (y < 0) y = h;
+        if (y > h) y = 0;
 
-        /*        ret = read(0, &ch, 1);
-        if ( !ret ) {
-            tcsetattr(0, TCSANOW, &restore);
-            fprintf(stderr, "getpos: error reading response!\n");
-            return 1;
-        }
-        buf[i] = ch;
-        printf("buf[%d]: \t%c \t%d\n", i, ch, ch);*/
-
-        read(fileno(stdin), buffer, 16);
-        fprintf(stdout, "%s", buffer);
-
+        // Render
         printf("\e[%d;%dH", y, x);
-        printf("\e[48;5;%dm ", t); // bg
+        printf("\e[48;5;%dm ", t % 255); // bg
         fflush(stdout);
         usleep(delay);
     };
