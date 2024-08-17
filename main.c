2cca0fb556c4f9472bb74e88c5bf962953c0f3b3
Mr Speaker <mrspeaker@gmail.com>
2024-08-17 20:06:44 +0800

    ok, bit far now

--- main.c
+++ main.c
@@ -5,17 +5,19 @@
 #include <termios.h>
 #include <unistd.h>
 
+#define C_BLACK 0
+#define C_WHITE 7
+#define delay 1000000 / 30
+
+unsigned char w = 0;
+unsigned char h = 0;
+struct winsize win;
+
 void esc(char* str) {
     printf("\e[%s", str);
 }
 
-char w = 0;
-char h = 0;
-
-struct winsize win;
-int stdoutid = STDOUT_FILENO;
-unsigned long twinsz = TIOCGWINSZ;
-
+// Set/restore non-canonical and no-echo in tty
 void init_tty(int enable) {
     struct termios ttystate;
     tcgetattr(STDIN_FILENO, &ttystate); // current state
@@ -32,7 +34,7 @@ void init_tty(int enable) {
     tcsetattr(STDIN_FILENO, TCSANOW, &ttystate);
 }
 
-// Simulates kbhit: wait 0 time for input
+// Simulates kbhit
 //
 // select() allows a program to monitor multiple file descriptors,
 // waiting until one or more of the file descriptors become "ready".
@@ -40,7 +42,7 @@ void init_tty(int enable) {
 int kbhit() {
     struct timeval tv;
     fd_set fds;
-    tv.tv_sec = 0;
+    tv.tv_sec = 0; // wait 0 time
     tv.tv_usec = 0;
     FD_ZERO(&fds);
     FD_SET(STDIN_FILENO, &fds);
@@ -48,28 +50,45 @@ int kbhit() {
     return FD_ISSET(STDIN_FILENO, &fds);
 }
 
-void init() {
-    init_tty(1);
-    esc("48;5;0m"); // bg black
+void cursor_to(char x, char y) {
+    printf("\e[%d;%dH", y, x);
+}
+
+void set_bg(unsigned char col) {
+    printf("\e[48;5;%dm", col);
+}
+void set_fg(unsigned char col) {
+    printf("\e[38;5;%dm", col);
+}
+
+void cls() {
+    set_bg(C_BLACK);
+    set_fg(C_WHITE);
     esc("2J"); // clear screen
+}
+
+void init() {
+    //    cls();
+    init_tty(1);
     esc("?25l"); // hide cursor
 }
 
 void done(int signum) {
     init_tty(0);
     esc("?25h"); // show cursor
-    esc("37m"); // fg to white
-    esc("48;5;0m"); // bg to black
+
+    set_bg(C_BLACK);
+    set_fg(C_WHITE);
 
     printf("\nbye\n");
     exit(signum);
 };
 
 void resize() {
-    init();
-    ioctl(stdoutid, twinsz, &win);
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &win);
     w = win.ws_col;
     h = win.ws_row;
+    cls();
 }
 
 int main() {
@@ -77,24 +96,42 @@ int main() {
     signal(SIGWINCH, resize);
 
     resize();
+    init();
 
     char x = w / 2;
-    char y = h  / 2;
+    char y = h / 2;
     char dx = 0;
     char dy = 0;
 
-    int delay = 1000000 / 30;
-    unsigned char t = 0;
+
+    unsigned int t = 0;
 
     char ch;
     int running = 1;
+
+    cursor_to(w / 2 - 10, h / 2);
+    printf("hello, world");
+
+    int oo = 0;
+    for (int j = 0; j <= h; j++) {
+        for (int i = 0; i <= w; i++) {
+            cursor_to(i,j);
+            set_bg((oo + i) % 10 + 232);
+            printf(" ");
+        }
+        oo++;
+    }
+
+
     while(running){
         // Input
         if (kbhit()) {
-            dx = 0;
+            dx = 0; // stop moving
             dy = 0;
             ch = fgetc(stdin);
-            if (ch == 'q') running = 0;
+
+            if (ch == 'q') running = 0; // quit
+
             if (ch == 'w') dy = -1;
             if (ch == 's') dy = 1;
             if (ch == 'd') dx = 1;
@@ -113,10 +150,13 @@ int main() {
         if (y > h) y = 0;
 
         // Render
-        printf("\e[%d;%dH", y, x);
-        printf("\e[48;5;%dm ", t % 255); // bg
+        cursor_to(x, y);
+        set_bg(t % 255);
+        printf(" ");
+
         fflush(stdout);
         usleep(delay);
     };
+    done(0);
     return 0;
 }
