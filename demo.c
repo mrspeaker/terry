ef5820d567c40da5fb7f738c0bd63db05a74c40f
Mr Speaker <mrspeaker@gmail.com>
2024-08-21 21:16:38 +0800

    more decorations

--- demo.c
+++ demo.c
@@ -32,23 +32,11 @@ void set_fg(uint8_t col) {
     printf("\e[38;5;%dm", col);
 }
 
-void cls() {
+void init() {
     set_bg(C_BLACK);
     set_fg(C_WHITE);
-    printf("\e2J"); // clear screen
-}
-
-void init_grid() {
-    for (uint8_t j = 0; j < ROWS; j++) {
-        for (uint8_t i = 0; i < COLS; i++) {
-            grid[j][i] = rand() % 20;
-        }
-    }
-}
-
-void init() {
+    printf("\e[2J"); // clear screen
     printf("\e[?25l"); // hide cursor
-    init_grid();
 }
 
 uint8_t get_cell(uint8_t x, uint8_t y) {
@@ -79,20 +67,20 @@ void update_grid() {
 }
 
 void render_grid() {
-    // TODO: instead of default green -> blue -> black, make it go:
-    //  white -> yellow -> red -> blue -> black for real fire-y look.
+    // NOTE: instead of green -> blue -> black using default palette,  make it go:
+    // white -> yellow -> red -> blue -> black for real fire-y look.
 
-    uint16_t top = h / 2 - 13;
+    uint16_t top = h / 2 - 10;
 
-    for (uint8_t j = 0; j < ROWS - 2; j+=2) {
+    for (uint8_t j = 0; j < ROWS - 3; j += 2) {
         cursor_to(w / 2 - 20, top + j / 2);
         for (uint8_t i = 0; i < COLS; i++) {
             uint8_t top = grid[j][i];
             uint8_t bottom = grid[j + 1][i];
 
             // Filter out a bunch o colors. (Gradient is indexes 16-51)
-            if (top < 16 || top > 51) top = C_BLACK;
-            if (bottom < 16 || bottom  > 51) bottom = C_BLACK;
+            if (top < 16) top = C_BLACK;
+            if (bottom < 16) bottom = C_BLACK;
 
             set_fg(top);
             set_bg(bottom);
@@ -111,8 +99,10 @@ void render_text(int t) {
     set_bg(C_BLACK);
     uint32_t col = t / 5;
     set_fg(col);
-    cursor_to(w / 2 - 7, h / 2 + 9);
+    cursor_to(w / 2 - 7, h / 2 + 12);
     printf("Mr\e[CSpeaker\e[C2024");
+    set_fg(C_BLACK); // hide cursor from user input
+    cursor_to(0,0);
 }
 
 // Starry starry background
@@ -132,13 +122,14 @@ void bg_fill() {
         }
     }
 
-    // Bottom row
-    set_fg(21);
-    cursor_to(w / 2 - 19, h / 2 + 9);
-    printf("▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔");
-    cursor_to(w / 2 - 19, h / 2 + 10);
-    printf("▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔");
-
+    // Bottom stripes
+    for (int i = 0; i < 4; i++) {
+        set_fg(21 - i);
+        cursor_to(w / 2 - 19, h / 2 + 11 + i);
+        printf(i == 3 ? " " : "▔");
+        printf("▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔");
+        printf(i == 3 ? " " : "▔");
+    }
 }
 
 void done(int signum) {
@@ -162,12 +153,10 @@ int main() {
     signal(SIGINT, done);
     signal(SIGWINCH, resize);
 
-    cls();
     init();
     resize();
 
     uint32_t t = 0;
-
     while(1){
         // Update
         t++;
